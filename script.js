42d615d88753619fc0f253a471ed4f3a2eee0c56
anuksha11 <118464546+anuksha11@users.noreply.github.com>
2023-06-27 19:09:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Script for navigation bar
+const bar=document.getElementById('bar')
+const nav=document.getElementById('navbar')
+const close=document.getElementById('close')
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
+
+var pro=document.querySelectorAll('.pro')
+for(allp of pro){
+    allp.addEventListener('click',()=>{
+        window.location.href='sproduct.html';
+    })
+}
